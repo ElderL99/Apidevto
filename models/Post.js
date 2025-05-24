523a076979035bf57bfa71b032dec9d87f2e3276
ElderL99 <adan.lugo.barrientos@outlook.com>
2025-05-23 21:07:05 -0600

    windows change

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
+const Reaction = require('./Reaction'); // Asegúrate de que la ruta sea correcta
+const User = require('./User'); // Asegúrate de que la ruta sea correcta
+
 
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  tags: { type: [String], default: [] }, // <-- Nuevo campo "tags"
+  tags: { type: [String], default: [] }, 
   createdAt: { type: Date, default: Date.now },
+  image: { type: String }, 
 });
 
 
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Conectado a MongoDB'))
